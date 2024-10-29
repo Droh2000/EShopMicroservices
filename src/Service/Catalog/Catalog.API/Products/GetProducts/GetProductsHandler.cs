ebffad01c02f695b9d249db618bb1ac24936fd46
diego ortega <diego-chido2011@hotmail.com>
2024-10-29 17:46:27 -0600

    Seccion 6 Completada

---
+++ src/Service/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsQuery() : IQuery<GetProductsResut>;
+
+    public record GetProductsResut(IEnumerable<Product> Products);
+
+    internal class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductsQuery, GetProductsResut>
+    {
+        public async Task<GetProductsResut> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler.Handler called with {@Query}", query);
+
+            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+
+            return new GetProductsResut(products);
+        }
+    }
+}
