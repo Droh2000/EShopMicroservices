c10446ff81f623dc8636ceb2b326730548dd9dd7
diego ortega <diego-chido2011@hotmail.com>
2024-11-02 14:26:33 -0600

    Seccion 8 Completada

---
+++ src/Service/Basket/Basket.API/Data/IBasketRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Basket.API.Data
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GatBasket(string userName, CancellationToken cancellationToken = default);
+        Task<ShoppingCart> StoreBasket(ShoppingCart shoppingCart, CancellationToken cancellationToken = default);
+        Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default);
+    }
+}
